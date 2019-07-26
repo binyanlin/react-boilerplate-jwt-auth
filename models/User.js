af10797faea7c8c5c1ff14b3beaefe1638ade0ee
Binyan Lin <obinyans@gmail.com>
2019-07-26 10:52:49 -0700

    added validator npm functionality

--- models/User.js
+++ models/User.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
+
+const validateEmail = function(email){
+  return validator.isEmail(email);
+}
 
 const UserSchema = new Schema({
   email: {
     type: String,
     unique: true,
     required: true,
-    lowercase: true
+    lowercase: true,
+    validate: [validateEmail, "Invalid Email"]
   },
   password: {
     type: String,
